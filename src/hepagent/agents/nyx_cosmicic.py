a33dc713de4a80ab868b08310caa4d262ea3789a
Xiangyang Ju <xju@lbl.gov>
2025-12-25 22:37:09 -0800

    cosmic ic

--- src/hepagent/agents/nyx_cosmicic.py
+++ src/hepagent/agents/nyx_cosmicic.py
@@ -1,32 +1,20 @@
-from agents import Agent, Runner
-from hepagent.model_providers import get_cborg_model_provider
+from agents import Runner
 from hepagent.agent_helpers import print_usage
-
-
-def create() -> Agent:
-    agent = Agent(
-        name="Comisic Initial Condition Generator",
-        instructions=(
-            "You are good at compiling and running the cosmic initial"
-            "condition generator `cosmicic`."
-            "Users have to provide a local copy of the code: git@bitbucket-lbnl:zarija/cosmicic.git"
-        ),
-        model=get_cborg_model_provider(),
-        tools=[],
-    )
-    return agent
+from hepagent.agents.bash import create as create_bash_agent
 
 
 async def main():
-    task_prompt = """My cosmicic code is located at /pscratch/sd/x/xju/FoundationUniverse/nyx_sim/cosmicic
-    Please make a copy of the code to /pscratch/sd/x/xju/FoundationUniverse/nyx_sim/agent_area/v0/cosmicic
-    and compile it. If the compilation is successful, copy the executable `init` to
-    the work directory: /pscratch/sd/x/xju/FoundationUniverse/nyx_sim/agent_area. run it to generate initial conditions.
+    task_prompt = """Your working directory is /pscratch/sd/x/xju/FoundationUniverse/nyx_sim/agent_area/v0.
+    The original cosmicic code is located at /pscratch/sd/x/xju/FoundationUniverse/nyx_sim/cosmicic.
+    Make a copy of cosmicic code to your working directory, compile it.
+    If the compilation is successful, copy the executable `init` to your working directory,
+    where you will use it to generate initial conditions.
     Note that if the platform is Perlmuttter, you need to load these module first:
     - cray-fftw
     - cray-hdf5-parallel
+    Finally, tell me if cosmic initial condition generation is ready.
 """
-    agent = create()
+    agent = create_bash_agent()
     result = await Runner.run(agent, task_prompt)
     print(result.final_output)
     print_usage(result.context_wrapper.usage)
